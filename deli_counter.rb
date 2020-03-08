e0b213f91ff6f7a8c605e7f16b6f24057110a5d3
Amal Abdu <abduamal@ymail.com>
2020-03-08 00:33:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli)
+  katz_deli = []
+  counter = 0
+  
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif i < katz_deli.length 
+    
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+  katz_deli = []
+  katz_deli.push(name)
+  return "Welcome, #{name}. You are currently #{katz_deli.size} in line."
+end
+
+
+def now_serving
+
+end

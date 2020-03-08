b254ae2681be1d56d0339899c376e23da297222b
Amal Abdu <abduamal@ymail.com>
2020-03-08 03:05:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,6 @@
 def line(katz_deli)
   line = []
-  i = 0
   
-  if katz_deli.length == 0 
-    puts "The line is currently empty."
-  elsif i < katz_deli.length 
-    line += (i + 1) + ". " + katz_deli[i] + ", "
-  end
-  line = line.insert(0, line.length-2)
-  return "The line is currently: " + line
 end
 
 
